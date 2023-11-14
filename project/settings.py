1f66d2341317ad187044eaf9d4c3357cf3526dad
Preetham987 <120121413+Preetham987@users.noreply.github.com>
2023-11-14 15:58:10 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
           'ENGINE': 'django.db.backends.postgresql',
           'NAME': 'djangodb',
           'USER': 'postgres',
-          'PASSWORD': 'Preetham@28',
+          'PASSWORD': '123456',
           'HOST': '127.0.0.1',
           'PORT': '5432'
     }
